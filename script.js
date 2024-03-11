4e97a1c096ed7dffe984e8f594977226ac5b1627
Rex0Liang <159222539+Rex0Liang@users.noreply.github.com>
2024-03-11 14:13:55 +1100

    Start laying out game

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let myIntro = 15;
-let enterName = 15;
+let enterName = prompt("Enter your Name");
+let playerFeedback = "test";
 
-if (myIntro == enterName) {
-    alert(myIntro);
+
+loadDIV ();
+
+function loadDIV () {
+document.getElementById("ScriptThisDIV").innerHTML = "<h1>Hello " + enterName;
+document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "<h2>Guess a number between 1 & 10</h2>";
+document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + playerFeedback;
+
 }
